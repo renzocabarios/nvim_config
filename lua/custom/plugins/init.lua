0312d0644330985068a057358286064ed821c5bd
renzocabarios <renzo.cabarios@gmail.com>
2024-09-08 04:17:32 +0800

    Add config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,68 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'ThePrimeagen/vim-be-good',
+    version = '*',
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {--[[ things you want to change go here]]
+      direction = 'float',
+    },
+    -- keys = {
+    -- { '<leader>td', '<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    -- },
+    config = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd 'startinsert!'
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd 'startinsert!'
+        end,
+      }
+
+      local term = Terminal:new {
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd 'startinsert!'
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd 'startinsert!'
+        end,
+      }
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      function _term_toggle()
+        term:toggle()
+      end
+
+      vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>lua _term_toggle()<CR>', { noremap = true, silent = true })
+    end,
+  },
+}
